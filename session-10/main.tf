f2bca717884f5f7978e41dbd5ca2dac7d85cbf63
hikmatillomuhtorov04 <hikmatillomuhtorov04@gmail.com>
2022-11-04 00:27:34 +0000

    added session-11 folder

--- session-10/main.tf
+++ session-10/main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-  source        = "github.com/HikmatulloMuhtorov/terraform-session-october/modules/ec2"
+  source        = "github.com/HikmatulloMuhtorov/terraform-session-october//modules/ec2?ref=v1.0.0"
   env           = "dev"
   instance_type = "t2.micro"
 }
